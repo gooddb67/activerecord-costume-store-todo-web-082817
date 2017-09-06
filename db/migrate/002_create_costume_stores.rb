1024975da339b35a9796e7b8f68986238aeb1577
Dave Goodman <davegoodman20@gmail.com>
2017-09-06 17:41:50 -0400

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,18 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration
+# By convention, the class name should match the part of the
+# file name after the number, so in this case:
+# 002_create_costumes.rb becomes class CreateCostumes
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.float :num_of_employees
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
+      t.timestamps
+    end
+  end
+end
